eaeba795a182dc5d72a99d8eb51463b05fae09e6
Divit Khullar <divit.khullar@nagarro.com>
2023-03-03 14:15:45 +0530

    config changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import Router from 'next/router';
 import { I18nProvider } from 'next-localization';
 import NProgress from 'nprogress';
 import { SitecorePageProps } from 'lib/page-props';
-import '../assets/login.css';
 
 // Using bootstrap and nprogress are completely optional.
 //  bootstrap is used here to provide a clean layout for samples, without needing extra CSS in the sample app
