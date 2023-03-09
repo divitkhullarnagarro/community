35829f870bb0bd4c730eaae4834ea07a991e562d
Nishant Tiwari <nishant.tiwari@nagarro.com>
2023-03-09 16:01:26 +0530

    AddTextBlog

--- src/acceleratorcommunity/src/pages/dashboard.tsx
+++ src/acceleratorcommunity/src/pages/dashboard.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import Dashboard from '../components/Dashboard';
+import AddPost from '../components/AddPost';
 
 function dashboard() {
-  return <Dashboard />;
+  let tempVar: any;
+
+  return (
+    <>
+      <Dashboard />
+      <AddPost
+        rendering={tempVar}
+        params={tempVar}
+        fields={{
+          heading: tempVar,
+        }}
+      />
+    </>
+  );
 }
 
 export default dashboard;
