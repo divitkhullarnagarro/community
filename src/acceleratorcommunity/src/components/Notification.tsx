46b68f80be32e58dd1d0f87b516cea172413c5b0
Rohit kumar <Rohit.kumar09@nagarro.com>
2023-04-24 12:17:40 +0530

    Notification service url updated and not used dropdown items removed

--- src/acceleratorcommunity/src/components/Notification.tsx
+++ src/acceleratorcommunity/src/components/Notification.tsx
@@ -10,14 +10,13 @@ import NotificationPerson from '../assets/images/NotificiationPerson.png';
 import ToastNotification from './ToastNotification';
 import NotificationLike from '../assets/images/NotificationLike.png';
 import NotificationComment from '../assets/images/NotificationComment.png';
-import NotificationCheck from '../assets/images/NotificationCheck.png';
 import NotificationOpen from '../assets/images/NotificationOpen.png';
-import NotificationSettings from '../assets/images/NotificationSettings.png';
 
 const NotificationType = {
   LIKE_ON_POST: 'LIKE_ON_POST',
   COMMENT_ON_POST: 'COMMENT_ON_POST',
   REPLY_ON_COMMENT: 'REPLY_ON_COMMENT',
+  POST_AN_ARTICLE_PEER: 'POST_AN_ARTICLE_PEER',
 };
 
 type NotificationProps = ComponentProps & {
@@ -42,24 +41,6 @@ type BaseNotificationType = {
 type ArticleNotificationType = BaseNotificationType & {
   articleId: string;
 };
-
-const dummyNotificationList: ArticleNotificationType[] = [
-  {
-    articleId: '375c4f2e-75d3-4a45-8005-ceb90f884aff',
-    message: 'guptashivam199228@gmail.com liked your post',
-    type: 'LIKE_ON_POST',
-  },
-  {
-    articleId: '375c4f2e-75d3-4a45-8005-ceb90f884aff',
-    message: 'You have a comment on your article by guptashivam199228@gmail.com',
-    type: 'COMMENT_ON_POST',
-  },
-  {
-    articleId: 'c94a957e-40ed-4be7-bdd2-420fc5b38a15',
-    message: 'You have a reply on your comment from shiv.gupta011@gmail.com',
-    type: 'REPLY_ON_COMMENT',
-  },
-];
 
 const Notification = (props: NotificationProps): JSX.Element => {
   const { datasource } = props?.fields?.data;
@@ -96,10 +77,6 @@ const Notification = (props: NotificationProps): JSX.Element => {
     setNotificationCount(notificationList?.length);
   }, [notificationList]);
 
-  useEffect(() => {
-    setNotificationCount(dummyNotificationList?.length);
-  }, [dummyNotificationList]);
-
   const resetToastState = () => {
     setShowNofitication(!showNotification);
     setToastSuccess(false);
@@ -119,6 +96,15 @@ const Notification = (props: NotificationProps): JSX.Element => {
         ]);
         break;
       }
+      case NotificationType.POST_AN_ARTICLE_PEER: {
+        setToastSuccess(true);
+        setShowNofitication(true);
+        setToastMessage('You have new notification');
+        setNotificationList([
+          ...notificationList,
+          { articleId: '', message: data?.message, type: data?.type },
+        ]);
+      }
       default: {
         break;
       }
@@ -130,73 +116,9 @@ const Notification = (props: NotificationProps): JSX.Element => {
       <>
         <div className={notificationCss.notificationHeaderRow}>
           <div className={notificationCss.notificationHeaderLabel}>Notifications</div>
-          <Dropdown>
-            <Dropdown.Toggle
-              variant="secondary"
-              className={notificationCss.notificationMoreOptions}
-            >
-              <button
-                style={{
-                  border: 'none',
-                  backgroundColor: 'white',
-                  padding: '0',
-                }}
-              >
-                <img
-                  className="postMoreOptionsImage"
-                  src="https://cdn-icons-png.flaticon.com/512/463/463292.png"
-                  alt="pan"
-                />
-              </button>
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu className={notificationCss.dropdownMoreMenu}>
-              <Dropdown.Item>
-                <div>
-                  <NextImage
-                    field={NotificationCheck}
-                    editable={true}
-                    width={15}
-                    height={15}
-                    title="Notification"
-                  ></NextImage>
-                  <span className={notificationCss.dropdownMenuOptionsLabel}>Mark all as read</span>
-                </div>
-              </Dropdown.Item>
-              <Dropdown.Item>
-                <div>
-                  <NextImage
-                    field={NotificationSettings}
-                    editable={true}
-                    width={15}
-                    height={15}
-                    title="Notification"
-                  ></NextImage>
-                  <span className={notificationCss.dropdownMenuOptionsLabel}>
-                    Notifications settings
-                  </span>
-                </div>
-              </Dropdown.Item>
-              <Dropdown.Item>
-                <div>
-                  <NextImage
-                    field={NotificationOpen}
-                    editable={true}
-                    width={15}
-                    height={15}
-                    title="Notification"
-                  ></NextImage>
-                  <span className={notificationCss.dropdownMenuOptionsLabel}>
-                    Open notifications
-                  </span>
-                </div>
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
         </div>
         <div className={notificationCss.notificationHeaderAction}>
           <Button className={notificationCss.notificationActionActiveBtn}>All</Button>
-          <Button className={notificationCss.notificationActionBtn}>Unread</Button>
         </div>
       </>
     );
@@ -232,68 +154,49 @@ const Notification = (props: NotificationProps): JSX.Element => {
             <div className={notificationCss.notificationMessage}>{item.message}</div>
           </div>
         </Dropdown.Item>
-        <Dropdown>
-          <Dropdown.Toggle variant="secondary" className={notificationCss.notificationMoreOptions}>
-            <button
-              style={{
-                border: 'none',
-                backgroundColor: 'white',
-                padding: '0',
-              }}
+        {item?.type !== NotificationType.POST_AN_ARTICLE_PEER ? (
+          <Dropdown>
+            <Dropdown.Toggle
+              variant="secondary"
+              className={notificationCss.notificationMoreOptions}
             >
-              <img
-                className="postMoreOptionsImage"
-                src="https://cdn-icons-png.flaticon.com/512/463/463292.png"
-                alt="pan"
-              />
-            </button>
-          </Dropdown.Toggle>
+              <button
+                style={{
+                  border: 'none',
+                  backgroundColor: 'white',
+                  padding: '0',
+                }}
+              >
+                <img
+                  className="postMoreOptionsImage"
+                  src="https://cdn-icons-png.flaticon.com/512/463/463292.png"
+                  alt="pan"
+                />
+              </button>
+            </Dropdown.Toggle>
 
-          <Dropdown.Menu className={notificationCss.dropdownMoreMenuForNotificationItem}>
-            <Dropdown.Item>
-              <div>
-                <NextImage
-                  field={NotificationCheck}
-                  editable={true}
-                  width={15}
-                  height={15}
-                  title="Notification"
-                ></NextImage>
-                <span className={notificationCss.dropdownMenuOptionsLabel}>Mark as unread</span>
-              </div>
-            </Dropdown.Item>
-            <Dropdown.Item>
-              <div>
-                <NextImage
-                  field={NotificationSettings}
-                  editable={true}
-                  width={15}
-                  height={15}
-                  title="Notification"
-                ></NextImage>
-                <span className={notificationCss.dropdownMenuOptionsLabel}>
-                  Remove this notification
-                </span>
-              </div>
-            </Dropdown.Item>
-            <Dropdown.Item
-              href={`/post/${item.articleId}`}
-              target="_blank"
-              className={notificationCss.dropdownItem}
-            >
-              <div>
-                <NextImage
-                  field={NotificationOpen}
-                  editable={true}
-                  width={15}
-                  height={15}
-                  title="Notification"
-                ></NextImage>
-                <span className={notificationCss.dropdownMenuOptionsLabel}>View post</span>
-              </div>
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
+            <Dropdown.Menu className={notificationCss.dropdownMoreMenuForNotificationItem}>
+              <Dropdown.Item
+                href={`/post/${item.articleId}`}
+                target="_blank"
+                className={notificationCss.dropdownItem}
+              >
+                <div>
+                  <NextImage
+                    field={NotificationOpen}
+                    editable={true}
+                    width={15}
+                    height={15}
+                    title="Notification"
+                  ></NextImage>
+                  <span className={notificationCss.dropdownMenuOptionsLabel}>View post</span>
+                </div>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        ) : (
+          <></>
+        )}
       </div>
     );
   };
@@ -327,8 +230,8 @@ const Notification = (props: NotificationProps): JSX.Element => {
             <Dropdown.Header className={notificationCss.notificationHeader}>
               <NotificationBodyHeader />
             </Dropdown.Header>
-            {dummyNotificationList?.length > 0 ? (
-              dummyNotificationList.map((item) => {
+            {notificationList?.length > 0 ? (
+              notificationList.map((item) => {
                 return <NotificationRow {...item} />;
               })
             ) : (
