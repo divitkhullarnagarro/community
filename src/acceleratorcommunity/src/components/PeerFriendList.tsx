fb55d7a0a23d72b2d5a21c07d4519c186a348a2e
Rohit kumar <Rohit.kumar09@nagarro.com>
2023-05-01 15:28:44 +0530

    Peer friend list tab added in profile page

--- src/acceleratorcommunity/src/components/PeerFriendList.tsx
+++ src/acceleratorcommunity/src/components/PeerFriendList.tsx
@@ -7,6 +7,9 @@ import WebContext from 'src/Context/WebContext';
 import Skeleton from 'react-loading-skeleton';
 import Profile from '../assets/images/ProfilePic.jpeg';
 import allPeersCall from 'src/API/getPeers';
+import { Button, Card } from 'react-bootstrap';
+import DropArrow from '../assets/images/DropArrow.png';
+import FollowUnfollowButton from './FollowUnfollowButton';
 
 type PeerFriendListProps = ComponentProps & {
   fields: {
@@ -31,6 +34,7 @@ const PeerFriendList = (props: PeerFriendListProps): JSX.Element => {
   console.log('isWidgetView', isWidgetView);
 
   const getAllPears = async () => {
+    setIsDataLoaded(false);
     const response = await allPeersCall(userToken);
     if (response?.data) {
       setIsDataLoaded(true);
@@ -49,7 +53,10 @@ const PeerFriendList = (props: PeerFriendListProps): JSX.Element => {
       <div className={styles.wrapper}>
         <div className={styles.header}>
           <div className={styles.heading}>{'Peers'}</div>
-          <Link href={{ pathname: '/profile' }} className={styles.linkHeader}>
+          <Link
+            href={{ pathname: '/profile', query: 'showTab=peers' }}
+            className={styles.linkHeader}
+          >
             <span className={styles.link}> {'See All'}</span>
           </Link>
         </div>
@@ -67,6 +74,9 @@ const PeerFriendList = (props: PeerFriendListProps): JSX.Element => {
                   />
                   <div>
                     <div className={styles.name}>{item?.firstName + ' ' + item?.lastName}</div>
+                  </div>
+                  <div className={styles.button}>
+                    <FollowUnfollowButton userName={item?.objectId} buttonText={'Unfollow'} />
                   </div>
                 </div>
               );
@@ -107,6 +117,72 @@ const PeerFriendList = (props: PeerFriendListProps): JSX.Element => {
     );
   };
 
+  const FullPagePeerFriendList = () => {
+    return (
+      <div className={styles.mainContainer}>
+        <div className={styles.container}>
+          <div className={styles.mainFullPageItemWrapper}>
+            <div className={styles.newgrid}>
+              {isDataLoaded ? (
+                peerFriendList?.length > 0 ? (
+                  peerFriendList.map((item) => {
+                    return <FullPagePeerFriendListItem {...item} />;
+                  })
+                ) : (
+                  <></>
+                )
+              ) : (
+                skeletonDummyArr.concat(skeletonDummyArr).map((item: any) => {
+                  return <FullPagePeerFriendListSkeletonItem {...item} />;
+                })
+              )}
+            </div>
+            {peerFriendList === undefined || numItems >= peerFriendList?.length ? null : (
+              <Button className={styles.seeMoreBtn} onClick={() => setNumItems(numItems + 6)}>
+                <div>
+                  <span className={styles.seeMoreBtn}>See more</span>
+                </div>
+                <NextImage field={DropArrow} editable={true} />
+              </Button>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  };
+  const [numItems, setNumItems] = useState(12);
+
+  const FullPagePeerFriendListItem = (item: peerFriendFields) => {
+    return (
+      <Card className={styles.cardItem}>
+        <div className={styles.imageContainer}>
+          <img className={styles.imgProfile} contentEditable={true} src={Profile.src} />
+        </div>
+        <Card.Body>
+          <Card.Title className={styles.cardTitle}>
+            {item?.firstName + ' ' + item?.lastName}
+          </Card.Title>
+          <FollowUnfollowButton userName={item?.objectId} buttonText={'Unfollow'} />
+        </Card.Body>
+      </Card>
+    );
+  };
+
+  const FullPagePeerFriendListSkeletonItem = (index: any) => {
+    return (
+      <Card key={index} className={styles.cardItem}>
+        <div className={styles.imageContainer}>
+          <Skeleton height={260} />
+        </div>
+        <Card.Body>
+          <Card.Title className={styles.cardTitle}>
+            <Skeleton height={40} />
+          </Card.Title>
+        </Card.Body>
+      </Card>
+    );
+  };
+
   return (
     <>
       {isWidgetView ? (
@@ -116,7 +192,7 @@ const PeerFriendList = (props: PeerFriendListProps): JSX.Element => {
           <WidgetViewPeerFriendListSkeleton />
         )
       ) : (
-        <></>
+        <>{<FullPagePeerFriendList />}</>
       )}
     </>
   );
