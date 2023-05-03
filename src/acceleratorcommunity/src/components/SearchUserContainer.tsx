63de7bc810d589c750af2d7327f8ae32d227f85b
anmolchawla810 <anmol.chawla@nagarro.com>
2023-05-03 17:15:20 +0530

    made skeleton for search components

--- src/acceleratorcommunity/src/components/SearchUserContainer.tsx
+++ src/acceleratorcommunity/src/components/SearchUserContainer.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import User from './User';
 import styles from '../assets/searchFilterContainer.module.css';
+import { SearchSkeletonForUser } from './skeletons/SearchSkeleton';
 
-const SearchUserContainer = () => {
+const SearchUserContainer = (props: any) => {
   return (
     <div className={styles.parentContainer}>
       <div className={styles.generalcontainer}>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => {
-          return <User />;
-        })}
+        {props?.success ? (
+          <SearchSkeletonForUser count={5} />
+        ) : (
+          [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => {
+            return <User />;
+          })
+        )}
       </div>
     </div>
   );
