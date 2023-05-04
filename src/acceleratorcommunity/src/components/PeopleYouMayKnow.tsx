9a7311e9e72cf9a9c50cf1096e362f4545e4365d
Vicky Dhakar <vicky.dhakar@nagarro.com>
2023-05-04 16:37:39 +0530

    fix: profile changes

--- src/acceleratorcommunity/src/components/PeopleYouMayKnow.tsx
+++ src/acceleratorcommunity/src/components/PeopleYouMayKnow.tsx
@@ -145,7 +145,9 @@ const PeopleYouMayKnow = (props: PeopleYouMayKnowProps): JSX.Element => {
     return (
       <Card className={styles.cardItem}>
         <div className={styles.imageContainer}>
-          <img className={styles.imgProfile} contentEditable={true} src={Profile.src} />
+          <Link href={`/profile/${item?.objectId}`}>
+            <img className={styles.imgProfile} contentEditable={true} src={Profile.src} />
+          </Link>
         </div>
         <Card.Body>
           <Card.Title className={styles.cardTitle}>
