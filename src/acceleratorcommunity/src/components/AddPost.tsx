04a3b1179071f64246fd7b6191fb75f8f91a9f55
Nishant Tiwari <nishant.tiwari@nagarro.com>
2023-05-04 17:33:33 +0530

    Add-Post-Merge

--- src/acceleratorcommunity/src/components/AddPost.tsx
+++ src/acceleratorcommunity/src/components/AddPost.tsx
@@ -39,7 +39,7 @@ import pollIcon from '../assets/images/CreatePoll_icon.svg';
 import addBlogIcon from '../assets/images/AddBlogPost_icon.svg';
 import createEventIcon from '../assets/images/CreateEventPost_icon.svg';
 import pollCross from '../assets/images/pollCross.svg';
-import Skeleton from 'react-loading-skeleton';
+// import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 // Rich Text Editor Files Import Start
