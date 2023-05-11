8d7f6835123f162962333be0fe7a4e70f6d24399
Vicky Dhakar <vicky.dhakar@nagarro.com>
2023-05-11 18:18:32 +0530

    fix:logo changes

--- src/acceleratorcommunity/src/components/Logo.tsx
+++ src/acceleratorcommunity/src/components/Logo.tsx
@@ -1,18 +1,22 @@
-import { NextImage } from '@sitecore-jss/sitecore-jss-nextjs';
+// import { NextImage } from '@sitecore-jss/sitecore-jss-nextjs';
+import { LogoInterface } from 'assets/helpers/interfaces/logo';
+import Image from 'next/image';
 import Link from 'next/link';
 import logo from '../assets/images/CommunityLogo.svg';
 import LogoStyles from '../assets/logo.module.css';
 
-const Logo = (): JSX.Element => {
+const Logo = (props: LogoInterface): JSX.Element => {
+  console.log('logoProps', props.fields.data.datasource);
+  const { image, logoURL } = props?.fields?.data?.datasource;
   return (
     <div className={LogoStyles.container}>
-      <Link href="/" passHref={true}>
-        <NextImage
+      <Link href={`${logoURL?.jsonValue?.value?.href}`} passHref={true}>
+        <Image
           style={{ cursor: 'pointer' }}
-          field={logo}
-          editable={true}
-          height={45}
-          width={45}
+          src={image?.jsonValue?.value?.src ? image?.jsonValue?.value?.src : logo}
+          // editable={true}
+          height={80}
+          width={80}
         />
       </Link>
     </div>
