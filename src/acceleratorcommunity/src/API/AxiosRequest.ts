0b4978457ab39024d80d1386d11aa4d69510855f
Vicky Dhakar <vicky.dhakar@nagarro.com>
2023-05-26 17:46:03 +0530

    feat: issue fixed register error handler

--- src/acceleratorcommunity/src/API/AxiosRequest.ts
+++ src/acceleratorcommunity/src/API/AxiosRequest.ts
@@ -5,43 +5,45 @@ type Method = 'GET' | 'POST' | 'PUT' | 'DELETE';
 type Headers = { [key: string]: string };
 
 interface RequestConfig extends AxiosRequestConfig {
-    method: Method;
-    url: string;
-    headers?: Headers;
-    data?: any;
-    params?: any;
-    contentType?: string;
+  method: Method;
+  url: string;
+  headers?: Headers;
+  data?: any;
+  params?: any;
+  contentType?: string;
 }
 
 async function AxiosRequest<T>(config: RequestConfig): Promise<T> {
-    const { method, url, headers, data, params, contentType } = config;
+  const { method, url, headers, data, params, contentType } = config;
 
-    // Get the authorization token from the localStorage and Decrypt
-    let token = '' as any;
-    if (typeof localStorage !== 'undefined') {
-        token = localStorage.getItem('UserToken');
-        token ? (token = decryptString(token)) : '';
-    }
+  // Get the authorization token from the localStorage and Decrypt
+  let token = '' as any;
+  if (typeof localStorage !== 'undefined') {
+    token = localStorage.getItem('UserToken');
+    token ? (token = decryptString(token)) : '';
+  }
 
-    try {
-        const response = await axios({
-            method,
-            url,
-            headers: {
-                // Always include the authorization header with the bearer token
-                ...(headers || {}),
-                'Content-Type': contentType || 'application/json',
-                Authorization: `Bearer ${token}`,
-            },
-            data,
-            params,
-        });
+  try {
+    const response = await axios({
+      method,
+      url,
+      headers: {
+        // Always include the authorization header with the bearer token
+        ...(headers || {}),
+        'Content-Type': contentType || 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      data,
+      params,
+    });
+    console.log('errorincall', response);
+    return response.data;
+  } catch (error: any) {
+    console.log('errorincall', error);
+    return error.response;
+  }
 
-        return response.data;
-    } catch (error: any) {
-        console.log(error.message);
-    }
-    return Promise.reject("API Call Failed !");
+  //   return Promise.reject('API Call Failed !');
 }
 
 export default AxiosRequest;
